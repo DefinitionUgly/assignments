95ee61875aa78ca6668be09aba34d24bd5bf81a5
Ian Nalyanya <highmeme10@gmail.com>
2019-10-12 14:25:06 +0300

    First commit

---
+++ insert.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    char *front;
+    char data;
+    char *rear;
+};
+    
+struct node p;
+struct node q;
+struct node linked[5];
+
+int main()
+{
+
+
+    char search;
+
+    char data[5];
+    int i;
+    for(i=0;i<5;i++){
+        printf("Enter Character: \n");
+        scanf("%c%*c",&data[i]);
+
+        q.data = data[i];
+
+        if(i == 0){
+
+            q.front = NULL;
+            q.rear = NULL;
+
+        }else{
+
+        q.front = p.rear;
+                
+        
+        }
+        //printf("%p",&data[i]);
+        ///P = Q
+        p.front = q.front;
+        p.data = q.data;
+        if(i == 4){
+            p.rear = NULL;
+        }else
+        {
+           p.rear = &data[i];///Same as &q.data;
+ 
+        }
+        
+        
+       //printf("%c\n",data[i]);
+        //printf("\n%p %c %p\n",p.front,p.data,p.rear);
+        linked[i] = p;
+    }
+
+
+    //printf("\nEnter Character to search for: \n");
+    //scanf("%c%*c",&search);
+
+    for(i=0;i<5;i++){
+        printf("\n%p %c %p\n",linked[i].front,linked[i].data,linked[i].rear);
+        
+    }
+
+    printf('Enter value to search for');
+    scanf("%c",&search);
+    
+    for(i= 0;i<5;i++){
+        
+    }
+    
+    
+
+
+
+
+    return 0;
+}
